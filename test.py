9b087e15e4fad9b129933220eb468069564e1ec1
Sebastian Linke <Seb_Linke@arcor.de>
2013-09-28 11:56:45 +0200

    Add tests for `sort_items`.

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
+import functools
+import locale
 import shcol
 import unittest
+
 
 class TestColumnize(unittest.TestCase):
     def _columnize(self, *args, **kwargs):
@@ -9,6 +15,17 @@ class TestColumnize(unittest.TestCase):
 
     def _join(self, items, spacing=2):
         return (spacing * ' ').join(items)
+
+    def _sorted(self, items, locale_to_use=None):
+        if locale_to_use is None:
+            return sorted(items)
+        old_locale = locale.getlocale(locale.LC_COLLATE)
+        sortkey = functools.cmp_to_key(locale.strcoll)
+        try:
+            locale.setlocale(locale.LC_COLLATE, locale_to_use)
+            return sorted(items, key=sortkey)
+        finally:
+            locale.setlocale(locale.LC_COLLATE, old_locale)
 
     def test_no_items(self):
         self.assertEqual(self._columnize([]), '')
@@ -34,6 +51,19 @@ class TestColumnize(unittest.TestCase):
             self._columnize(items, max_line_width=45), '\n'.join([x, y, z])
         )
 
+    def test_sort_items(self):
+        items = ['spam', 'ham', 'egg', 'foo', 'bar', 'baz']
+        self.assertEqual(
+            self._columnize(items, sort_items=True),
+            self._join(self._sorted(items))
+        )
+        items = ['spam', 'häm', 'egg', 'späm', 'ham', 'ägg']
+        german_locale = ('de_DE', 'UTF-8')
+        self.assertEqual(
+            self._columnize(items, sort_items=True),
+            self._join(self._sorted(items, german_locale))
+        )
+        
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
