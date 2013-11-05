987f3ee8ad48cd075a11f1219be34e22c8853705
Sebastian Linke <Seb_Linke@arcor.de>
2013-11-05 01:15:44 +0100

    Add files needed by `distutils`.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import shcol
+
+setup(
+    name='shcol',
+    version=shcol.__version__,
+    description='A shell columnizer',
+    long_description=shcol.__doc__,
+    author=shcol.__author__,
+    author_email='Seb_Linke@arcor.de',
+    url='https://github.com/seblin/shcol',
+    py_modules=['shcol'],
+    license=shcol.__license__,
+    platforms = 'any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: System :: Shells',
+    ],
+)
