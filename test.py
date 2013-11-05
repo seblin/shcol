a29c89510bb1d225c7acac0470de2806a50221fa
Sebastian Linke <Seb_Linke@arcor.de>
2013-11-05 12:00:34 +0100

    Made a test in `.test_iter_lines()` more readable.

--- test.py
+++ test.py
@@ -124,7 +124,7 @@ class FormatterTest(unittest.TestCase):
         items = ['foo', 'bar', 'baz']
         lines = self._get_lines(items)
         self.assertEqual(len(lines), 1)
-        self.assertEqual(lines[0], self._join(items))
+        self.assertEqual(lines, [self._join(items)])
         self.formatter.column_width_calculator.max_line_width = 3
         self.assertEqual(self._get_lines(items), items)
 
