3e3ebc156bce39c4fee774d4d9cd3e1cc1584663
Sebastian Linke <Seb_Linke@arcor.de>
2014-02-12 01:05:48 +0100

    Add `hide_dotted`-option to `show_files()`.

--- shcol/highlevel.py
+++ shcol/highlevel.py
@@ -7,5 +7,8 @@ __all__ = ['show_attrs', 'show_files']
 def show_attrs(obj):
     print(columnize(dir(obj), sort_items=True))
 
-def show_files(path='.'):
-    print(columnize(os.listdir(path), sort_items=True))
+def show_files(path='.', hide_dotted=False):
+    filenames = os.listdir(path)
+    if hide_dotted:
+        filenames = [fn for fn in filenames if not fn.startswith('.')]
+    print(columnize(filenames, sort_items=True))
