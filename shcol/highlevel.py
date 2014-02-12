002ca7623f98d2dc046e8aea9854b315ff7588fd
Sebastian Linke <Seb_Linke@arcor.de>
2014-02-12 11:33:49 +0100

    Imitate some shell expansion in `show_files()`.

--- shcol/highlevel.py
+++ shcol/highlevel.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from .core import columnize
+import glob
 import os
 
 __all__ = ['show_attrs', 'show_files']
@@ -7,8 +8,18 @@ __all__ = ['show_attrs', 'show_files']
 def show_attrs(obj):
     print(columnize(dir(obj), sort_items=True))
 
-def show_files(path='.', hide_dotted=False):
-    filenames = os.listdir(path)
+def _get_files(path, hide_dotted):
+    path = os.path.expanduser(os.path.expandvars(path))
+    try:
+        filenames = os.listdir(path)
+    except OSError as err:
+        filenames = glob.glob(path)
+        if not filenames:
+            raise err
     if hide_dotted:
         filenames = [fn for fn in filenames if not fn.startswith('.')]
+    return filenames
+
+def show_files(path='.', hide_dotted=False):
+    filenames = _get_files(path, hide_dotted)
     print(columnize(filenames, sort_items=True))
