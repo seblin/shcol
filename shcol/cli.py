32f0335991ae105f2d6eaa0432730f2cb7961646
Sebastian Linke <Seb_Linke@arcor.de>
2014-02-27 11:53:06 +0100

    Reject negative numbers on CLI-level.

--- shcol/cli.py
+++ shcol/cli.py
@@ -6,7 +6,7 @@ import sys
 
 __all__ = ['main']
 
-def main(cmd_args):
+def main(cmd_args=None):
     parser = argparse.ArgumentParser(
         description='Generate columnized output for given string items.',
         version='shcol {}'.format(shcol.__version__)
@@ -17,17 +17,28 @@ def main(cmd_args):
     )
     parser.add_argument('items', nargs='*', metavar='item', help=item_help)
     parser.add_argument(
-        '-s', '--spacing', metavar='N', type=int, default=2,
+        '-s', '--spacing', metavar='N', type=num, default=2,
         help='number of blanks between two columns (default: 2)'
     )
     parser.add_argument(
-        '-w', '--width', metavar='N', type=int, default=80,
+        '-w', '--width', metavar='N', type=num, default=80,
         help='maximal amount of characters per line (default: 80)'
     )
     parser.add_argument(
         '-S', '--sort', action='store_true', default=False,
         help='sort the items'
     )
-    args = parser.parse_args(cmd_args[1:])
-    items = args.items or [line.rstrip('\n') for line in sys.stdin]
+    args = parser.parse_args(cmd_args)
+    items = args.items or _read_lines(sys.stdin)
     print(shcol.columnize(items, args.spacing, args.width, args.sort))
+
+
+def num(s):
+    number = int(s)
+    if number <= 0:
+        raise argparse.ArgumentError('number must be non-negative')
+    return number
+
+def _read_lines(stream):
+    for line in stream:
+        yield line.rstrip('\n')
