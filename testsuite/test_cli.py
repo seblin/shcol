51dd1eaf56e5392177838c896e2e3dc88e708357
Sebastian Linke <Seb_Linke@arcor.de>
2014-05-04 09:41:56 +0200

    Add test for `--column`-option.

--- testsuite/test_cli.py
+++ testsuite/test_cli.py
@@ -21,8 +21,6 @@ class CapturedStream(object):
         setattr(sys, self.stream_name, self.original_stream)
 
 class ArgumentParserTest(unittest.TestCase):
-    dummy_item = 'spam'
-
     def setUp(self):
         self.parser = shcol.cli.ArgumentParser('shcol', shcol.__version__)
 
@@ -41,25 +39,29 @@ class ArgumentParserTest(unittest.TestCase):
             result = self._get_stderr_output([version])
             self.assertEqual(result, expected)
 
-    def _test_num_option(self, long_option, short_option):
+    def _test_num_option(self, long_option, short_option, *cmd_args):
         option_name = long_option.lstrip('-')
-        dummy_item = 'spam'
         for value in ('0', '2', '80', '100'):
-            args = self.parser.parse_args([long_option, value, self.dummy_item])
+            args = self.parser.parse_args([long_option, value] + list(cmd_args))
             self.assertEqual(getattr(args, option_name), int(value))
+        args = self.parser.parse_args([short_option, '2'] + list(cmd_args))
+        self.assertEqual(getattr(args, option_name), 2)
+        self._test_num_option_invalid(short_option, long_option, *cmd_args)
+
+    def _test_num_option_invalid(self, short_option, long_option, *cmd_args):
         for invalid in ('-42', '-1', '1.0', 'x'):
             error = self._get_stderr_output(
-                [long_option, invalid, self.dummy_item]
+                [long_option, invalid] + list(cmd_args)
             )
             self.assertIn('invalid num value', error)
-        args = self.parser.parse_args([short_option, '2', self.dummy_item])
-        self.assertEqual(getattr(args, option_name), 2)
+        error = self._get_stderr_output([short_option, '-1'] + list(cmd_args))
+        self.assertIn('invalid num value', error)
 
     def test_spacing(self):
-        self._test_num_option('--spacing', '-s')
+        self._test_num_option('--spacing', '-s', 'foo')
 
     def test_line_width(self):
-        self._test_num_option('--width', '-w')
+        self._test_num_option('--width', '-w', 'foo')
 
     def test_item_args(self):
         items = ['spam', 'ham', 'eggs']
@@ -75,8 +77,16 @@ class ArgumentParserTest(unittest.TestCase):
             args = self.parser.parse_args([])
             self.assertEqual(args.items, ['spam', 'ham', 'eggs'])
 
+    def test_column(self):
+        self._test_num_option_invalid('-c', '--column')
+        with CapturedStream('stdin') as instream:
+            instream.write('xxx spam\nzzz ham\n~~~ eggs')
+            instream.seek(0)
+            args = self.parser.parse_args(['-c' '1'])
+            self.assertEqual(args.items, ['spam', 'ham', 'eggs'])
+
     def test_sort(self):
-        args = self.parser.parse_args([self.dummy_item])
+        args = self.parser.parse_args(['spam'])
         self.assertFalse(args.sort)
-        args = self.parser.parse_args(['--sort', self.dummy_item])
+        args = self.parser.parse_args(['--sort', 'spam'])
         self.assertTrue(args.sort)
