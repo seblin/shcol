7d2300e712d6217d4f0825e40392880185822592
Sebastian Linke <Seb_Linke@arcor.de>
2014-05-08 02:02:59 +0200

    Add test for `print_files()`.

--- testsuite/test_highlevel.py
+++ testsuite/test_highlevel.py
@@ -1,10 +1,11 @@
 import unittest
+import os
 import shcol
 
 from _helpers import CapturedStream
 
 class PrintTestBase(unittest.TestCase):
-    def get_result(self, func, *args, **kwargs):
+    def get_output(self, func, *args, **kwargs):
         with CapturedStream('stdout') as outstream:
             func(*args, **kwargs)
             return outstream.getvalue()
@@ -13,15 +14,35 @@ class PrintColumnizedTest(PrintTestBase):
     def test_items(self):
         items = ['spam', 'ham', 'eggs']
         expected = '  '.join(items) + '\n'
-        result = self.get_result(shcol.print_columnized, items)
+        result = self.get_output(shcol.print_columnized, items)
         self.assertEqual(result, expected)
 
     def test_no_items(self):
-        result = self.get_result(shcol.print_columnized, [])
+        result = self.get_output(shcol.print_columnized, [])
         self.assertEqual(result, '\n')
 
 class PrintAttrsTest(PrintTestBase):
     def test_print_attrs(self):
         expected = shcol.columnize(dir(shcol), sort_items=True) + '\n'
-        result = self.get_result(shcol.print_attrs, shcol)
+        result = self.get_output(shcol.print_attrs, shcol)
+        self.assertEqual(result, expected)
+
+class PrintFilesTest(PrintTestBase):
+    def test_get_files(self):
+        # TODO: Make this test smarter
+        expected = os.listdir('.')
+        result = shcol.highlevel._get_files('.', False)
+        self.assertEqual(result, expected)
+        expected = [fn for fn in expected if not fn.startswith('.')]
+        result = shcol.highlevel._get_files('.', True)
+        self.assertEqual(result, expected)
+
+    def test_print_files(self):
+        filenames = os.listdir('.')
+        expected = shcol.columnize(filenames, sort_items=True) + '\n'
+        result = self.get_output(shcol.print_files)
+        self.assertEqual(result, expected)
+        filenames = [fn for fn in filenames if not fn.startswith('.')]
+        expected = shcol.columnize(filenames, sort_items=True) + '\n'
+        result = self.get_output(shcol.print_files, hide_dotted=True)
         self.assertEqual(result, expected)
