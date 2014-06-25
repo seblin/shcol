1aae241e98e700c15965e0dbff481b427067c5ee
Sebastian Linke <Seb_Linke@arcor.de>
2014-06-26 00:18:41 +0200

    Various changes: * Introduce `helpers`-package * Reflow and rewrite some docstrings in `core` * Changes to formatter API * Rename `Formatter` to `SequenceFormatter` * Introduce `build_formatter()`

---
+++ shcol/helpers/misc.py
@@ -0,0 +1,70 @@
+import collections
+import functools
+import locale
+import sys
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+__all__ = [
+    'StringIO', 'STRING_TYPES', 'get_decoded', 'get_sorted',
+    'DefaultLocale', 'CapturedStream'
+]
+
+try:
+    STRING_TYPES = (str, bytes, unicode)
+except NameError:
+    STRING_TYPES = (str, bytes)
+
+def get_decoded(items, encoding):
+    for item in items:
+        if not isinstance(item, STRING_TYPES):
+            raise TypeError('all items must be strings')
+        if isinstance(item, bytes):
+            item = item.decode(encoding)
+        yield item
+
+def get_sorted(items, sortkey=None):
+    if sortkey is None:
+        sortkey = functools.cmp_to_key(locale.strcoll)
+    with DefaultLocale(locale.LC_COLLATE):
+        if isinstance(items, collections.Mapping):
+            result = sorted(items.items(), key=sortkey)
+            return collections.OrderedDict(result)
+        return sorted(items, key=sortkey)
+
+
+class DefaultLocale(object):
+    def __init__(self, category, fail_on_locale_error=False):
+        self.category = category
+        self.fail_on_locale_error = fail_on_locale_error
+        self.old_locale = None
+
+    def __enter__(self):
+        self.old_locale = locale.getlocale(self.category)
+        try:
+            locale.setlocale(self.category, '')
+        except locale.Error as err:
+            if self.fail_on_locale_error:
+                raise err
+
+    def __exit__(self, *unused):
+        if self.old_locale is not None:
+            locale.setlocale(self.category, self.old_locale)
+        self.old_locale = None
+
+
+class CapturedStream(object):
+    def __init__(self, stream_name):
+        self.stream_name = stream_name
+        self.original_stream = getattr(sys, stream_name)
+        self.pseudo_stream = StringIO()
+
+    def __enter__(self):
+        setattr(sys, self.stream_name, self.pseudo_stream)
+        return self.pseudo_stream
+
+    def __exit__(self, *unused):
+        setattr(sys, self.stream_name, self.original_stream)
---
+++ shcol/helpers/termwidth.py
@@ -0,0 +1,68 @@
+import ctypes
+import os
+import platform
+import sys
+
+__all__ = ['get_terminal_width']
+
+if hasattr(os, 'get_terminal_size'):
+    def get_terminal_width(fd):
+        # New in Python >= 3.3
+        return os.get_terminal_size(fd).columns
+
+elif platform.system() == 'Windows':
+    import ctypes.wintypes
+
+    class ConsoleScreenBufferInfo(ctypes.Structure):
+        _fields_ = [
+            ('dwSize', ctypes.wintypes._COORD),
+            ('dwCursorPosition', ctypes.wintypes._COORD),
+            ('wAttributes', ctypes.wintypes.WORD),
+            ('srWindow', ctypes.wintypes.SMALL_RECT),
+            ('dwMaximumWindowSize', ctypes.wintypes._COORD)
+        ]
+        _pack_ = 2
+
+    GetStdHandle = ctypes.windll.kernel32.GetStdHandle
+    GetStdHandle.restype = ctypes.wintypes.HANDLE
+    GetStdHandle.argtypes = [ctypes.wintypes.DWORD]
+
+    GetConsoleScreenBufferInfo = (
+        ctypes.windll.kernel32.GetConsoleScreenBufferInfo
+    )
+    GetConsoleScreenBufferInfo.restype = ctypes.wintypes.BOOL
+    GetConsoleScreenBufferInfo.argtypes = [
+        ctypes.wintypes.HANDLE,
+        ctypes.POINTER(ConsoleScreenBufferInfo),
+    ]
+
+    def get_terminal_width(fd):
+        num_handle = -(10 + fd)
+        handle = GetStdHandle(num_handle)
+        csbi = ctypes.pointer(ConsoleScreenBufferInfo())
+        GetConsoleScreenBufferInfo(handle, csbi)
+        window = csbi.contents.srWindow
+        return window.Right - window.Left + 1
+
+else:
+    try:
+        import fcntl
+        import termios
+    except ImportError:
+        pass
+
+    class WinSize(ctypes.Structure):
+        _fields_ = [
+            ('ws_row', ctypes.c_ushort),
+            ('ws_col', ctypes.c_ushort),
+            ('ws_xpixel', ctypes.c_ushort),
+            ('ws_ypixel', ctypes.c_ushort)
+        ]
+
+    def get_terminal_width(fd):
+        if not 'termios' in sys.modules or not hasattr(termios, 'TIOCGWINSZ'):
+            raise OSError('unsupported platform')
+        result = fcntl.ioctl(
+            fd, termios.TIOCGWINSZ, ctypes.sizeof(WinSize) * '\0'
+        )
+        return WinSize.from_buffer_copy(result).ws_col
