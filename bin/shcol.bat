6d89d4bc26df51cb6a83ec40d75264594ab827d9
seblin <Seb_Linke@arcor.de>
2015-02-05 20:02:08 +0100

    Add copyright note and description to Windows-specific starter.

--- bin/shcol.bat
+++ bin/shcol.bat
@@ -1,1 +1,8 @@
+:: Copyright (c) 2013-2015, Sebastian Linke
+
+:: Released under the Simplified BSD license
+:: (see LICENSE file for details).
+
+:: A starter to invoke `shcol` from the Windows command-line.
+
 @python -m shcol %*
